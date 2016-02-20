c0b17e8d06502e8dc9f4c4c51878344462c40157
Jiří Němec <klerik@klerik.cz>
2016-02-20 16:52:48 +0100

    tak uz to vypisuje json databazi :)

--- index.py
+++ index.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import cherrypy
+from tinydb import TinyDB, where
+
+db = TinyDB('/home/klerik/Downloads/muj_blog/db.json')
+
+kategorie = db.table('kategorie')
+zapisky=db.table('zapisky')
 
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return "Hello world!"
-
-testik = cherrypy.sessio
-
-test = HelloWorld()
+        return str(zapisky.all())
 
 if __name__ == '__main__':
    cherrypy.quickstart(HelloWorld())
